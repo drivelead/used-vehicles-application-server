c14475d85ab57b531b6ee2f71b740c849e8c3bd9
Azhar Zaman <68331974+azharzaman1@users.noreply.github.com>
2023-03-03 21:39:06 +0500

    mongodb srv string updated

--- db/mongo.js
+++ db/mongo.js
@@ -4,7 +4,7 @@ import config from "../config/env.js";
 const connectMongo = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://autolead-used-showroom:JTFIYBm9yFRR0hH4@autolead-used-showroom.pyut55l.mongodb.net/autolead_used_showroom?retryWrites=true&w=majority",
+      "mongodb+srv://newuser1:DF8PDUbCA4fnMsrP@next-mern-stack.ze4uf.mongodb.net/autolead_used_showroom?retryWrites=true&w=majority",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
