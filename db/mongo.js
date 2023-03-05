5ff2977286c6a771facadc95adec4cd870f5a3fa
Azhar Zaman <68331974+azharzaman1@users.noreply.github.com>
2023-03-05 03:00:45 -0800

    aws build pipeline push 1

--- db/mongo.js
+++ db/mongo.js
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
-import config from "../config/env.js";
+// import config from "../config/env.js";
 
 const connectMongo = async () => {
   try {
-    await mongoose.connect(config.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      "mongodb+srv://newuser1:DF8PDUbCA4fnMsrP@next-mern-stack.ze4uf.mongodb.net/autolead_vms?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
   } catch (err) {
     console.log(err.message);
   }
--- server.js
+++ server.js
@@ -52,7 +52,7 @@ mongoose.connection.once("open", () => {
   console.log("Connected to MongoDB.");
 });
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3500;
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
 
